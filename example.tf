46681fea2c4ebeea1f6d89b43e246f8f6cc38ce5
Suresh Peram <suresh.peram@S05450-MBPR.local>
2020-01-16 09:07:29 -0800

    added example.tf

---
+++ example.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0f4a377c755a5e8ef"
+}
