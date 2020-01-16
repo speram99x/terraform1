e4bf1052abe3e034abf37c480075435caedb0593
Suresh Peram <suresh.peram@S05450-MBPR.local>
2020-01-16 15:53:22 -0800

    Now using lambda_function.py

--- example.tf
+++ example.tf
@@ -20,14 +20,14 @@ resource "aws_instance" "example" {
 
 data "archive_file" "s3-public-access-block-zip" {
   type        = "zip"
-  source_file = "${path.module}/lambdas/s3-public-access-block/lambda.py"
+  source_file = "${path.module}/lambdas/s3-public-access-block/lambda_function.py"
   output_path = "${path.module}/lambdas/s3-public-access-block/zipfile/s3-public-access-block.zip"
 }
 
 resource "aws_lambda_function" "s3_public_access_block_lambda" {
   function_name = "sp-s3-public-access-block-lambda-2"
   role          = "arn:aws:iam::533359187263:role/service-role/SP_ServiceRoleForConfigAndS3"
-  handler       = "lambda.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   filename      = "${path.module}/lambdas/s3-public-access-block/zipfile/s3-public-access-block.zip"
   memory_size = "256"
