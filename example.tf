a3b02cb659283f51f1e44800f9cd8b3e656a8b31
Suresh Peram <suresh.peram@S05450-MBPR.local>
2020-01-19 19:49:30 -0800

    Added role and environment variable

--- example.tf
+++ example.tf
@@ -6,11 +6,20 @@ variable "access_key" {
   type = string
 }
 
+#
+# TODO: change the authentication below as appropriate
+#
+
 provider "aws" {
   region     = "us-east-1"
   access_key = var.access_id
   secret_key = var.access_key
 }
+
+#
+# test with ec2 instance
+# TODO: Remove the following ec2 instance
+#
 
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
@@ -18,11 +27,246 @@ resource "aws_instance" "example" {
   subnet_id = "subnet-0f4a377c755a5e8ef"
 }
 
-#data "archive_file" "lambda_zip" {
-#    type          = "zip"
-#    source_file   = "index.js"
-#    output_path   = "lambda_function.zip"
-#}
+#
+# TODO: Change the name of the role as appropriate
+#
+resource "aws_iam_role" "sp-lambda-role" {
+  name = "sp-lambda-role"
+
+  assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+EOF
+}
+
+resource "aws_iam_policy" "sp-lambda-policy" {
+  name        = "sp-lambda-policy"
+  description = "Policy for Lambda Role"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+		"Sid": "Stmt-for-Lambda-Policy",
+		"Effect": "Allow",
+		"Action": [
+				"cloudwatch:*",
+                "config:BatchGet*",
+                "config:Describe*",
+                "config:Get*",
+                "config:List*",
+                "config:Put*",
+                "config:Select*",
+                "ec2:Describe*",
+                "ec2:AcceptVpcPeeringConnection",
+                "ec2:AcceptVpcEndpointConnections",
+                "ec2:AllocateAddress",
+                "ec2:AssignIpv6Addresses",
+                "ec2:AssignPrivateIpAddresses",
+                "ec2:AssociateAddress",
+                "ec2:AssociateDhcpOptions",
+                "ec2:AssociateRouteTable",
+                "ec2:AssociateSubnetCidrBlock",
+                "ec2:AssociateVpcCidrBlock",
+                "ec2:AttachClassicLinkVpc",
+                "ec2:AttachInternetGateway",
+                "ec2:AttachNetworkInterface",
+                "ec2:AttachVpnGateway",
+                "ec2:AuthorizeSecurityGroupEgress",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:CreateCustomerGateway",
+                "ec2:CreateDefaultSubnet",
+                "ec2:CreateDefaultVpc",
+                "ec2:CreateDhcpOptions",
+                "ec2:CreateEgressOnlyInternetGateway",
+                "ec2:CreateFlowLogs",
+                "ec2:CreateInternetGateway",
+                "ec2:CreateNatGateway",
+                "ec2:CreateNetworkAcl",
+                "ec2:CreateNetworkAcl",
+                "ec2:CreateNetworkAclEntry",
+                "ec2:CreateNetworkInterface",
+                "ec2:CreateNetworkInterfacePermission",
+                "ec2:CreateRoute",
+                "ec2:CreateRouteTable",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateSubnet",
+                "ec2:CreateTags",
+                "ec2:CreateVpc",
+                "ec2:CreateVpcEndpoint",
+                "ec2:CreateVpcEndpointConnectionNotification",
+                "ec2:CreateVpcEndpointServiceConfiguration",
+                "ec2:CreateVpcPeeringConnection",
+                "ec2:CreateVpnConnection",
+                "ec2:CreateVpnConnectionRoute",
+                "ec2:CreateVpnGateway",
+                "ec2:DeleteCustomerGateway",
+                "ec2:DeleteDhcpOptions",
+                "ec2:DeleteEgressOnlyInternetGateway",
+                "ec2:DeleteFlowLogs",
+                "ec2:DeleteInternetGateway",
+                "ec2:DeleteNatGateway",
+                "ec2:DeleteNetworkAcl",
+                "ec2:DeleteNetworkAclEntry",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DeleteNetworkInterfacePermission",
+                "ec2:DeleteRoute",
+                "ec2:DeleteRouteTable",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteSubnet",
+                "ec2:DeleteTags",
+                "ec2:DeleteVpc",
+                "ec2:DeleteVpcEndpoints",
+                "ec2:DeleteVpcEndpointConnectionNotifications",
+                "ec2:DeleteVpcEndpointServiceConfigurations",
+                "ec2:DeleteVpcPeeringConnection",
+                "ec2:DeleteVpnConnection",
+                "ec2:DeleteVpnConnectionRoute",
+                "ec2:DeleteVpnGateway",
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeAddresses",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeClassicLinkInstances",
+                "ec2:DescribeCustomerGateways",
+                "ec2:DescribeDhcpOptions",
+                "ec2:DescribeEgressOnlyInternetGateways",
+                "ec2:DescribeFlowLogs",
+                "ec2:DescribeInstances",
+                "ec2:DescribeInternetGateways",
+                "ec2:DescribeKeyPairs",
+                "ec2:DescribeMovingAddresses",
+                "ec2:DescribeNatGateways",
+                "ec2:DescribeNetworkAcls",
+                "ec2:DescribeNetworkInterfaceAttribute",
+                "ec2:DescribeNetworkInterfacePermissions",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribePrefixLists",
+                "ec2:DescribeRouteTables",
+                "ec2:DescribeSecurityGroupReferences",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeStaleSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeTags",
+                "ec2:DescribeVpcAttribute",
+                "ec2:DescribeVpcClassicLink",
+                "ec2:DescribeVpcClassicLinkDnsSupport",
+                "ec2:DescribeVpcEndpointConnectionNotifications",
+                "ec2:DescribeVpcEndpointConnections",
+                "ec2:DescribeVpcEndpoints",
+                "ec2:DescribeVpcEndpointServiceConfigurations",
+                "ec2:DescribeVpcEndpointServicePermissions",
+                "ec2:DescribeVpcEndpointServices",
+                "ec2:DescribeVpcPeeringConnections",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeVpnConnections",
+                "ec2:DescribeVpnGateways",
+                "ec2:DetachClassicLinkVpc",
+                "ec2:DetachInternetGateway",
+                "ec2:DetachNetworkInterface",
+                "ec2:DetachVpnGateway",
+                "ec2:DisableVgwRoutePropagation",
+                "ec2:DisableVpcClassicLink",
+                "ec2:DisableVpcClassicLinkDnsSupport",
+                "ec2:DisassociateAddress",
+                "ec2:DisassociateRouteTable",
+                "ec2:DisassociateSubnetCidrBlock",
+                "ec2:DisassociateVpcCidrBlock",
+                "ec2:EnableVgwRoutePropagation",
+                "ec2:EnableVpcClassicLink",
+                "ec2:EnableVpcClassicLinkDnsSupport",
+                "ec2:ModifyNetworkInterfaceAttribute",
+                "ec2:ModifySubnetAttribute",
+                "ec2:ModifyVpcAttribute",
+                "ec2:ModifyVpcEndpoint",
+                "ec2:ModifyVpcEndpointConnectionNotification",
+                "ec2:ModifyVpcEndpointServiceConfiguration",
+                "ec2:ModifyVpcEndpointServicePermissions",
+                "ec2:ModifyVpcPeeringConnectionOptions",
+                "ec2:ModifyVpcTenancy",
+                "ec2:MoveAddressToVpc",
+                "ec2:RejectVpcEndpointConnections",
+                "ec2:RejectVpcPeeringConnection",
+                "ec2:ReleaseAddress",
+                "ec2:ReplaceNetworkAclAssociation",
+                "ec2:ReplaceNetworkAclEntry",
+                "ec2:ReplaceRoute",
+                "ec2:ReplaceRouteTableAssociation",
+                "ec2:ResetNetworkInterfaceAttribute",
+                "ec2:RestoreAddressToClassic",
+                "ec2:RevokeSecurityGroupEgress",
+                "ec2:RevokeSecurityGroupIngress",
+                "ec2:UnassignIpv6Addresses",
+                "ec2:UnassignPrivateIpAddresses",
+                "ec2:UpdateSecurityGroupRuleDescriptionsEgress",
+                "ec2:UpdateSecurityGroupRuleDescriptionsIngress"
+                "iam:GetGroup",
+                "iam:GetGroupPolicy",
+                "iam:GetPolicy",
+                "iam:GetPolicyVersion",
+                "iam:GetRole",
+                "iam:GetRolePolicy",
+                "iam:GetUser",
+                "iam:GetUserPolicy",
+                "iam:ListAttachedGroupPolicies",
+                "iam:ListAttachedRolePolicies",
+                "iam:ListAttachedUserPolicies",
+                "iam:ListEntitiesForPolicy",
+                "iam:ListGroupPolicies",
+                "iam:ListGroupsForUser",
+                "iam:ListInstanceProfilesForRole",
+                "iam:ListPolicyVersions",
+                "iam:ListRolePolicies",
+                "iam:ListUserPolicies",
+                "s3:GetAccelerateConfiguration",
+                "s3:GetAccountPublicAccessBlock",
+                "s3:GetBucketAcl",
+                "s3:GetBucketCORS",
+                "s3:GetBucketLocation",
+                "s3:GetBucketLogging",
+                "s3:GetBucketNotification",
+                "s3:GetBucketObjectLockConfiguration",
+                "s3:GetBucketPolicy",
+                "s3:GetBucketPublicAccessBlock",
+                "s3:GetBucketRequestPayment",
+                "s3:GetBucketTagging",
+                "s3:GetBucketVersioning",
+                "s3:GetBucketWebsite",
+                "s3:GetEncryptionConfiguration",
+                "s3:GetLifecycleConfiguration",
+                "s3:GetObject",
+                "s3:GetReplicationConfiguration",
+                "s3:ListAllMyBuckets",
+                "s3:ListBucket",                
+				"s3:PutAccountPublicAccessBlock",
+				"lambda:InvokeFunction",		
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+		],
+		"Resource": "*"
+    }    
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-role-policy-attach" {
+  role       = "${aws_iam_role.sp-lambda-role.name}"
+  policy_arn = "${aws_iam_policy.sp-lambda-policy.arn}"
+}
+
 
 data "archive_file" "s3-public-access-block-zip2" {
   type        = "zip"
@@ -30,16 +274,29 @@ data "archive_file" "s3-public-access-block-zip2" {
   output_path = "${path.module}/lambdas/s3-public-access-block/zipfile/s3-public-access-block.zip"
 }
 
+#
+# TODO: Change the name of the central policy bucket as appropriate
+#
 resource "aws_lambda_function" "s3_public_access_block_lambda" {
   function_name = "sp-s3-public-access-block-lambda-2"
-  role          = "arn:aws:iam::533359187263:role/service-role/SP_ServiceRoleForConfigAndS3"
+  role          = aws_iam_role.sp-lambda-role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
-#  filename      = "${path.module}/lambdas/s3-public-access-block/lambda_function.zip"
   filename     = "${path.module}/lambdas/s3-public-access-block/zipfile/s3-public-access-block.zip"
   source_code_hash = "${data.archive_file.s3-public-access-block-zip2.output_base64sha256}"
   memory_size = "256"
   timeout     = "30"
+  
+  environment {
+    variables = {
+      "CENTRAL_POLICY_BUCKET" = "sp-central-policy-bucket"
+    }
+  }
+
+  depends_on = [
+  	"aws_iam_role_policy_attachment.lambda-role-policy-attach"
+  	]
+  	
 }
 
 resource "aws_lambda_permission" "example" {
@@ -50,7 +307,6 @@ resource "aws_lambda_permission" "example" {
 }
 
 resource "aws_config_config_rule" "test_rule_1" {
-  # ... other configuration ...
   name = "sp-check-s3-public-access-rule-2"
 
   scope {
@@ -69,8 +325,10 @@ resource "aws_config_config_rule" "test_rule_1" {
     }
   }
 
-  # depends_on = ["aws_config_configuration_recorder.example", "aws_lambda_permission.example"]
-  depends_on = ["aws_lambda_permission.example", "aws_config_configuration_recorder.main_recorder"]
+  depends_on = [
+  	"aws_lambda_permission.example",
+  	"aws_config_configuration_recorder.main_recorder"
+  	]
 }
 
 #
